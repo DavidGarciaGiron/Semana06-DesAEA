17626d50146e8e04216b8413eda9064afec8d5a7
DavidGarciaGiron <david.garcia@tecsup.edu.pe>
2019-09-23 21:07:30 -0500

    first commit

---
+++ Semana06/Semana04/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Business;
+using Entity;
+
+namespace Semana04
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConsultar_Click(object sender, RoutedEventArgs e)
+        {
+            BPedido bPedido = null;
+
+            try
+            {
+                bPedido = new BPedido();
+                dgvPedido.ItemsSource = bPedido.GetPedidosEntreFechas(Convert.ToDateTime(txtFechaInicio.Text),
+                    Convert.ToDateTime(txtFechaFin.Text));
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Comunicarse con el Administrador");
+            }
+            finally
+            {
+                bPedido = null;
+            }
+        }
+
+        private void DgvPedido_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BDetallePedido bDetallePedido = null;
+            try
+            {
+                int idpedido;
+                Pedido pedido = (Pedido)dgvPedido.SelectedItem;
+                idpedido = pedido.IdPedido;
+                //var item = dgvPedido.SelectedItems as DataRowView;
+                //if (item == null) return;
+                //idpedido = Convert.ToInt32(item.Row["IdPedido"]);
+                bDetallePedido = new BDetallePedido();
+                dgvDetallePedido.ItemsSource = bDetallePedido.GetDetallePedidosPorId(idpedido);
+                txtTotal.Text = bDetallePedido.GetDetalleTotalPorId(idpedido).ToString();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                bDetallePedido = null;
+            }
+        }
+    }
+}
